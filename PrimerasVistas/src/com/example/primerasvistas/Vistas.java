d823fa9a7d4ade884d0b1837b514a4865317cdf9
Marco Antonio Martin Callejo <marcomoguer@yahoo.es>
2013-02-11 05:55:58 +0100

    Curso de programación de aplicaciones en Android. Modulo 2
    "Los Layouts en Android, practica 1": Uso de un RelativeLayout.

---
+++ PrimerasVistas/src/com/example/primerasvistas/Vistas.java
@@ -0,0 +1,23 @@
+package com.example.primerasvistas;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Vistas extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_vistas);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_vistas, menu);
+		return true;
+	}
+
+}
